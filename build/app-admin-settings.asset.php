10752b76bafb63b88ed9ea11436598257c8f886a
Ronald Huereca <ronald@mediaron.com>
2024-08-28 23:54:45 -0500

    Doing prod build

--- build/app-admin-settings.asset.php
+++ build/app-admin-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '6d862dbf4df0aa01f86e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '695f679fa3e2c691c190');
